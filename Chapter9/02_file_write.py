dacad889d2be5f59ac27625c6d4649bcef48ecde
Deekshith <120660020+IAM-ROCK@users.noreply.github.com>
2025-11-12 22:01:18 +0530

    learning

---
+++ Chapter9/02_file_write.py
@@ -0,0 +1,7 @@
+st="Hey Deekshith you are amazing, Thank you"
+
+f=open("myfile.txt","w")
+
+f.write(st)
+
+f.close
---
+++ Chapter9/04_append.py
@@ -0,0 +1,7 @@
+st="Hey Deekshith you are amazing, Thank you"
+
+f=open("myfile.txt","a") # it fill add contents at the end of file
+
+f.write(st)
+
+f.close

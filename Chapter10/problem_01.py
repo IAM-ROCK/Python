d68b56333b167f8fdd98cd959fa5c9088854effa
Deekshith <120660020+IAM-ROCK@users.noreply.github.com>
2025-11-19 19:43:18 +0530

    solved

---
+++ Chapter10/problem_01.py
@@ -0,0 +1,14 @@
+class Programmer:
+    company="Microsoft"
+    def __init__(self,name,salary,pin):
+        self.name=name
+        self.salary=salary
+        self.pin=pin
+        
+p= Programmer("Deekshith",120000,517)
+print(p.name,p.salary,p.pin,p.company)     
+
+r= Programmer("Rohit",10000,5172)
+print(r.name,r.salary,r.pin,r.company)   
+        
+        

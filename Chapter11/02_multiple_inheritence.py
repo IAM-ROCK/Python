8e7d602570701715daeef5364f6a8ee139dc3e21
Deekshith <120660020+IAM-ROCK@users.noreply.github.com>
2025-11-22 13:56:24 +0530

    Multiple inheritence

---
+++ Chapter11/02_multiple_inheritence.py
@@ -0,0 +1,27 @@
+class Employee:
+    company="ITC"
+    name="Default name"
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.company}")
+
+class Coder:
+    language="Python"
+    def printLanguages(self):
+        print(f"Out of all languahes here is your language: {self.language}")
+
+
+class Programmer(Employee,Coder):
+    company="ITC Infotech"
+    def showLangauage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language")
+
+
+
+
+a=Employee()
+b=Programmer()
+
+print(a.company , b.company)
+b.show()
+b.printLanguages()
+b.showLangauage()
